7025a9bd0795b6266db027cddeed41d93e399714
RaineT07 <srt1849@rit.edu>
2024-03-31 20:18:26 -0400

    domomaker-C complete

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -6,9 +6,9 @@ const loginPage = (req, res) => res.render('login');
 const signupPage = (req, res) => res.render('signup');
 
 const logout = (req, res) => {
-    req.session.destroy();
-    res.redirect('/');
-}
+  req.session.destroy();
+  res.redirect('/');
+};
 const login = (req, res) => {
   const username = `${req.body.username}`;
   const pass = `${req.body.pass}`;
